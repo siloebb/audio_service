a3779491874551d1496c5536ce68551808285ff1
Siloe Bezerra Bispo <siloebb@gmail.com>
2021-05-19 15:31:32 -0300

    using FlutterFragmentActivityu

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -1,12 +1,12 @@
 package com.ryanheise.audioservice;
 
 import android.content.Context;
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.FlutterEngineCache;
 import io.flutter.embedding.engine.dart.DartExecutor;
 
-public class AudioServiceActivity extends FlutterActivity {
+public class AudioServiceActivity extends FlutterFragmentActivity {
     @Override
     public FlutterEngine provideFlutterEngine(Context context) {
         return AudioServicePlugin.getFlutterEngine(context);
